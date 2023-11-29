bea2e0ae803f1e8963ba68a635e444c282b22fd3
Gustavo <gustavopsousa15@gmail.com>
2023-11-29 20:26:55 -0300

    Inclusão tela de cadastro

--- lib/home.dart
+++ lib/home.dart
@@ -2,8 +2,7 @@ import 'package:outstock/Widgets/input.dart';
 import 'package:flutter/material.dart';
 import 'Widgets/botoes.dart';
 import 'TelaSelect.dart';
-
-
+import 'TelaCadastro.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -11,11 +10,8 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   final _usuario = TextEditingController();
   final _senha = TextEditingController();
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -30,30 +26,36 @@ class _HomeState extends State<Home> {
   }
 
   _body() {
-    return Container(alignment: Alignment.center,
-      child: Column(mainAxisAlignment: MainAxisAlignment.center,
-        children:<Widget>[
-          InputTextos("Usuário","Usuário",controller: _usuario),
-          InputTextos( "Senha","Senha",controller: _senha),
-          Botoes("Logar", onPressed: _click),
-      ]
-      )
-    );
+    return Container(
+        alignment: Alignment.center,
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              InputTextos("Usuário", "Usuário", controller: _usuario),
+              InputTextos("Senha", "Senha", controller: _senha),
+              Botoes("Logar", onPressed: _click),
+              Botoes("Criar", onPressed: _cadastro)
+            ]));
   }
 
-  void _click(){
+  void _click() {
     String usuario = _usuario.text;
     String senha = _senha.text;
     setState(() {
-      if (usuario == "001866" && senha == "40028922"){
-            Navigator.push(context,
-            MaterialPageRoute(
-                builder: ((context) => const TelaSelect())
-            )
-        );}
-        else() {
-        Text('Usuário ou senha incorreto');
-      };
-    });}
-}
+      if (usuario == "001866" && senha == "40028922") {
+        Navigator.push(context,
+            MaterialPageRoute(builder: ((context) => const TelaSelect())));
+      } else
+        () {
+          Text('Usuário ou senha incorreto');
+        };
+    });
+  }
 
+  void _cadastro() {
+    setState(() {
+      Navigator.push(context,
+          MaterialPageRoute(builder: ((context) => const TelaCadastro())));
+    });
+  }
+}
